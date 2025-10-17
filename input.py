1d13991aced5ff4da1c2f3979c4cbf9832d5a710
Mansi Unge <mansiunge@gmail.com>
2025-10-17 22:42:40 +0530

    input statement learned

---
+++ input.py
@@ -0,0 +1,12 @@
+# learning input statements
+
+# input statement for string/char
+name = input("enter you name : ")
+print("My name is",name)
+
+# input statement for int/number datatype
+age = int(input("enter your age : "))
+print("my age is", age)
+
+weight = float(input("enter you weight : "))
+print("my weight is", weight)
