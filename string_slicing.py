921ec4ea860c43bd2c16011b1d6ee4b6965e6ccf
Mansi Unge <mansiunge@gmail.com>
2025-10-19 19:49:55 +0530

    string slicing

---
+++ string_slicing.py
@@ -0,0 +1,11 @@
+name = "mansi unge"
+
+# slicing string 
+first_name = name[0:6]
+# first_name = name[ :6] --- thsi is same as [0:6]
+first_name1 = name[0:]  # this is same as [0:10]
+# first_name1 = name[0:]  # this is same as [0:10]
+print(first_name)
+print(first_name1)
+character1 = name[-1]
+print(character1)
