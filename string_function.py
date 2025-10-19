1866c5e0824e40a6dcf7e85a7849f526d444eaa3
Mansi Unge <mansiunge@gmail.com>
2025-10-19 22:21:11 +0530

    string function / methods

---
+++ string_function.py
@@ -0,0 +1,10 @@
+name = "mansi"
+name2 = "UNGE"
+print(len(name))
+print(name.endswith("si"))  #will give o/p as true
+print(name.endswith("rry"))  #will give o/p as false
+print(name.startswith("ma"))  #will give o/p as true
+print(name.startswith("vi"))  #will give o/p as false
+print(name.capitalize())
+print(name.upper())
+print(name2.lower())
