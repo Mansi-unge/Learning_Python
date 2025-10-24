d58d28e1b9073be12cbf4590711dd686c78eafb1
Mansi Unge <mansiunge@gmail.com>
2025-10-24 07:16:30 +0530

    list program

---
+++ list.py
@@ -0,0 +1,11 @@
+fruits = ["apple" , "banana" , "orange" , "strawberry"]
+print(fruits[0])
+fruits[0] = "mango"
+print(fruits)
+fruits.sort()
+print(fruits)
+no = [20,45,13,92,73,8]
+# no.sort()
+# print(no)
+no.reverse()
+print(no)
