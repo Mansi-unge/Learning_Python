d80196495d6daab70108937e05775c8f9d872200
Mansi Unge <mansiunge@gmail.com>
2025-10-25 12:26:55 +0530

    learned if else

---
+++ if_elif_else_ladder.py
@@ -0,0 +1,11 @@
+# program to try if elif else statement 
+
+
+age = int(input("enter your age : "))
+
+if(age <= 0):
+  print("the age you entered is invalid....!")
+elif(age >= 18):
+  print("you are eligible to vote....!")
+else:
+  print("you are not eligible to vote....!")
